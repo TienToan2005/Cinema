fe17e5fe0aa359658a42fa355f9005988a419eb4
Hoàng Tiến Toàn <toan01022005@gmail.com>
2026-03-29 15:03:52 +0700

    CORS

--- backend/src/main/java/TienToan/example/Cinema/DTO/response/UserResponse.java
+++ backend/src/main/java/TienToan/example/Cinema/DTO/response/UserResponse.java
@@ -2,6 +2,16 @@ package TienToan.example.Cinema.DTO.response;
 
 import lombok.Builder;
 
+import java.time.LocalDate;
+
 @Builder
 public class UserResponse {
+    private String fullName;
+    private String phoneNumber;
+    private String email;
+    private LocalDate birthday;
+    private String gender;
+    private String region;
+    private String district;
+    private String favoriteCinema;
 }
--- backend/src/main/java/TienToan/example/Cinema/security/SecurityConfig.java
+++ backend/src/main/java/TienToan/example/Cinema/security/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -49,5 +51,15 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:5173") // URL của Vite
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedHeaders("*");
+        }
     }
 }
